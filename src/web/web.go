897998533f832fab22372c4e5cd85a258eb7821a
Lucas Blanchard <lucas.blanchard@postman.com>
2024-08-07 18:41:03 -0500

    modularize web response internally

--- src/web/web.go
+++ src/web/web.go
@@ -13,87 +13,71 @@ const (
 )
 
 func ExplainCommand(command string, language string) (result string, err error) {
-	var data map[string]interface{}
-
 	// convert to JSON for proper escaping of strings which may be in the command
 	req := map[string]string{
 		"command":  command,
 		"language": language,
 	}
 	reqJSON, _ := json.Marshal(req)
-	response, err := http.Post(explainUrl, "application/json", bytes.NewBuffer(reqJSON))
 
+	response, msg, err := reqFlows(explainUrl, reqJSON)
 	if err != nil {
-		return "Failed to send response", err
+		return msg, err
 	}
 	defer response.Body.Close()
 
-	err = json.NewDecoder(response.Body).Decode(&data)
+	result, msg, err = parseResponse(response)
 	if err != nil {
-		return "Failed to parse Flows API response: %s\n", err
-	}
-
-	result, ok := data["output"].(string)
-	if !ok {
-		return "", errors.New("expected output field not found in Flows API response")
+		return msg, err
 	}
 
 	return result, nil
 }
 
 func GenerateCommand(prompt string, language string) (result string, err error) {
-	var data map[string]interface{}
-
 	// convert to JSON for proper escaping of strings which may be in the command
 	req := map[string]string{
 		"prompt":   prompt,
 		"language": language,
 	}
 	reqJSON, _ := json.Marshal(req)
-	response, err := http.Post(commandGenerationUrl, "application/json", bytes.NewBuffer(reqJSON))
 
+	response, msg, err := reqFlows(commandGenerationUrl, reqJSON)
 	if err != nil {
-		return "Failed to send response", err
+		return msg, err
 	}
 	defer response.Body.Close()
 
-	err = json.NewDecoder(response.Body).Decode(&data)
+	result, msg, err = parseResponse(response)
 	if err != nil {
-		return "Failed to parse Flows API response", err
-	}
-
-	result, ok := data["output"].(string)
-	if !ok {
-		return "", errors.New("expected output field not found in Flows API response")
+		return msg, err
 	}
 
 	return result, nil
 }
 
-func PromptAI(apiUrl string, prompt string, language string) (res *http.Response, err error) {
-	// convert to JSON for proper escaping of strings which may be in the command
-	req := map[string]string{
-		"prompt":   prompt,
-		"language": language,
-	}
-	reqJSON, _ := json.Marshal(req)
+func reqFlows(apiUrl string, reqJSON []byte) (res *http.Response, msg string, err error) {
 	res, err = http.Post(apiUrl, "application/json", bytes.NewBuffer(reqJSON))
-	return res, err
-}
-
-func ParseResponse(res *http.Response) (result string, err error) {
-	var data map[string]interface{}
-	err = json.NewDecoder(res.Body).Decode(&data)
 
 	if err != nil {
-		return "Failed to parse Flows API response: %s\n", err
+		return nil, "Failed to send response", err
+	}
+
+	return res, "", err
+}
+
+func parseResponse(res *http.Response) (result string, msg string, err error) {
+	var data map[string]interface{}
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return "", "Failed to parse Flows API response", err
 	}
 
 	result, ok := data["output"].(string)
-
 	if !ok {
-		return "", errors.New("expected output field not found in Flows API response")
+		return "", "", errors.New("expected output field not found in Flows API response")
 	}
 
-	return result, nil
+	return result, "", err
 }
