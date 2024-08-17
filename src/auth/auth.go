04f05af83e287a520141435e0bb9a026d84eba05
Lucas Blanchard <lucas.blanchard@postman.com>
2024-08-16 20:45:25 -0400

    add jwt verification + tests

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 )
 
 const (
-	registerUrl = "https://5709c812-4c6b-4bab-af35-a1e5a1413eda.mock.pstmn.io/register"
+	registerUrl = "https://add9d90f-2d32-483d-835f-3dd2cb814764.mock.pstmn.io/register"
+	verifyUrl   = "https://add9d90f-2d32-483d-835f-3dd2cb814764.mock.pstmn.io/verify"
 )
 
 type RequestRegister struct {
@@ -17,6 +20,20 @@ type RequestRegister struct {
 
 type ResponseRegister struct {
 	Output map[string]string
+}
+
+type RequestVerify struct {
+	Input string
+}
+
+type ResponseVerify struct {
+	Output struct {
+		Valid bool `json:"valid"`
+		Flid  struct {
+			Flid    string `json:"flid"`
+			Version string `json:"version"`
+		} `json:"flid"`
+	} `json:"Output"`
 }
 
 func RegisterIp(ip string) (result string, err error) {
@@ -37,6 +54,33 @@ func RegisterIp(ip string) (result string, err error) {
 	}
 
 	return result, nil
+}
+
+func VerifyJwt(jwt string) (result bool, flid string, version string, err error) {
+	req := RequestVerify{
+		jwt,
+	}
+	reqJSON, _ := json.Marshal(req)
+
+	fmt.Println("JWT: ", string(reqJSON))
+
+	response, _, err := reqFlows(verifyUrl, reqJSON)
+	if err != nil {
+		return false, "", "", err
+	}
+	defer response.Body.Close()
+
+	var parsedResponse ResponseVerify
+	parsedResponse, _, err = parseVerifyJwt(response)
+	if err != nil {
+		return false, "", "", err
+	}
+
+	result = parsedResponse.Output.Valid
+	flid = parsedResponse.Output.Flid.Flid
+	version = parsedResponse.Output.Flid.Version
+
+	return result, flid, version, nil
 }
 
 func reqFlows(apiUrl string, reqJSON []byte) (res *http.Response, msg string, err error) {
@@ -63,4 +107,18 @@ func parseIpRegister(res *http.Response) (result string, msg string, err error) 
 	}
 
 	return data.Output["jwt"], "", err
+}
+
+func parseVerifyJwt(res *http.Response) (data ResponseVerify, msg string, err error) {
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return data, "Failed to parse Flows API response", err
+	}
+
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		return data, "Failed to parse Flows API response", err
+	}
+
+	return data, "", err
 }
--- src/auth/auth_test.go
+++ src/auth/auth_test.go
@@ -10,7 +10,7 @@ func TestRegisterIp(t *testing.T) {
 	expected := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcCI6IjEuMS4xLjMiLCJmbGlkIjoiZGZjM2YzZjMtYjMyNy00NGZiLWJlYWMtZTY4ZTY0MmIyZThhIiwidmVyc2lvbiI6InYxIn0=.P5JWgTAJU91QPfLPGItu715fKOc1ImwWxJWL+FLb24g="
 
 	if result != expected || err != nil {
-		t.Fatalf(`VerifyIp(%s) = ("%s",%v). Expected ('%s',%v)`, ip, result, err, expected, nil)
+		t.Fatalf(`RegisterIp(%s) = ("%s",%v). Expected ('%s',%v)`, ip, result, err, expected, nil)
 	}
 }
 
@@ -19,6 +19,30 @@ func TestRegisterIpFail(t *testing.T) {
 	_, err := RegisterIp(ip)
 
 	if err == nil {
-		t.Fatalf(`VerifyIp(%s) = should raise an error`, ip)
+		t.Fatalf(`RegisterIp(%s) = should raise an error`, ip)
+	}
+}
+
+func TestVerifyJwt(t *testing.T) {
+	jwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcCI6IjEuMS4xLjMiLCJmbGlkIjoiZGZjM2YzZjMtYjMyNy00NGZiLWJlYWMtZTY4ZTY0MmIyZThhIiwidmVyc2lvbiI6InYxIn0=.P5JWgTAJU91QPfLPGItu715fKOc1ImwWxJWL+FLb24g="
+	result, flid, version, err := VerifyJwt(jwt)
+	expectedResult := true
+	expectedFlid := "ada20f6a-b0a6-4aa9-ac13-d8a4ccc25167"
+	expectedVersion := "v1"
+
+	if result != expectedResult || flid != expectedFlid || version != expectedVersion || err != nil {
+		t.Fatalf(`VerifyJwt(%s) = (%v, %s, %s, %v). Expected (%v, %s, %s, %v)`, jwt, result, flid, version, err, expectedResult, expectedFlid, expectedVersion, nil)
+	}
+}
+
+func TestVerifyJwtFail(t *testing.T) {
+	jwt := "false jwt"
+	result, flid, version, err := VerifyJwt(jwt)
+	expectedResult := false
+	expectedFlid := ""
+	expectedVersion := ""
+
+	if result != expectedResult || flid != expectedFlid || version != expectedVersion || err != nil {
+		t.Fatalf(`VerifyJwt(%s) = (%v, %s, %s, %v). Expected (%v, %s, %s, %v)`, jwt, result, flid, version, err, expectedResult, expectedFlid, expectedVersion, nil)
 	}
 }
