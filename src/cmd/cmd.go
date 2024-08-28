9f12104cddada6f5b6acee85fe8543a8b40b926b
Rodric Rabbah <rodric.rabbah@postman.com>
2024-08-27 21:46:40 -0400

    Fix config writes, hide -e for now

--- src/cmd/cmd.go
+++ src/cmd/cmd.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -14,10 +15,13 @@ type FlagConfig struct {
 	Login                  bool
 	Config                 bool
 	Prompt                 string
-	FLID                   string
+
+	AutoExecuteConf bool
+	LangtoolConf    string
+	FLID            string
 }
 
-func ParseCommandLine(args []string, flags *FlagConfig) {
+func ParseCommandLine(args []string, flags *FlagConfig) error {
 	rootCmd := &cobra.Command{
 		Use:   "fl <prompt>",
 		Short: "A command-line tool for generating command line scripts using AI",
@@ -51,41 +55,45 @@ func ParseCommandLine(args []string, flags *FlagConfig) {
 	rootCmd.PersistentFlags().BoolVarP(&flags.Verbose, "verbose", "v", false, "Verbose output")
 
 	rootCmd.PersistentFlags().BoolVarP(&flags.PromptRun, "prompt", "p", false, "Prompt to run generated commands")
-	configCmd.PersistentFlags().BoolVarP(&flags.AutoExecute, "run", "r", false, "Configure fl to always run the generated command without prompting")
-	rootCmd.PersistentFlags().BoolVarP(&flags.Explain, "explain", "e", false, "Explain the generated command")
+	rootCmd.PersistentFlags().BoolVarP(&flags.AutoExecute, "run", "r", flags.AutoExecuteConf, "Configure fl to always run the generated command without prompting")
+	//TODO
+	//rootCmd.PersistentFlags().BoolVarP(&flags.Explain, "explain", "e", false, "Explain the generated command")
 
 	rootCmd.PersistentFlags().StringVarP(&flags.Outfile, "outfile", "o", "", "Write generated command to file")
-	rootCmd.PersistentFlags().StringVarP(&flags.Langtool, "langtool", "l", "", "Generate command for specific shell or a tool")
+	rootCmd.PersistentFlags().StringVarP(&flags.Langtool, "langtool", "l", flags.LangtoolConf, "Generate command for specific shell or a tool")
 
-	configCmd.PersistentFlags().BoolVarP(&flags.AutoExecute, "run", "r", false, "Configure fl to always run the generated command without prompting")
-	configCmd.PersistentFlags().StringVarP(&flags.Langtool, "langtool", "l", "", "Set default shell or a tool or use")
+	configCmd.PersistentFlags().BoolVarP(&flags.AutoExecuteConf, "run", "r", flags.AutoExecuteConf, "Configure fl to always run the generated command without prompting")
+	configCmd.PersistentFlags().StringVarP(&flags.LangtoolConf, "langtool", "l", flags.LangtoolConf, "Set default shell or a tool or use")
 
 	rootCmd.SetArgs(args)
-	rootCmd.Execute()
+	return rootCmd.Execute()
 }
 
 func ReadConfig(filepath string, flags *FlagConfig) error {
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		return nil
+	}
+
 	viper.SetConfigFile(filepath)
 	viper.SetConfigType("json")
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		return err
 	}
 
-	flags.AutoExecute = viper.GetBool("run")
-	flags.Langtool = viper.GetString("langtool")
+	flags.AutoExecuteConf = viper.GetBool("run")
+	flags.LangtoolConf = viper.GetString("langtool")
 	flags.FLID = viper.GetString("flid")
+
 	return nil
 }
 
-func WriteConfig(filepath string, flags FlagConfig) error {
-	viper.SetConfigFile(filepath)
-	viper.SetConfigType("json")
-
-	viper.Set("run", flags.AutoExecute)
-	viper.Set("langtool", flags.Langtool)
+func WriteConfig(filepath string, flags *FlagConfig) error {
+	viper.Set("run", flags.AutoExecuteConf)
+	viper.Set("langtool", flags.LangtoolConf)
 	viper.Set("flid", flags.FLID)
 
+	viper.SetConfigFile(filepath)
+	viper.SetConfigType("json")
 	return viper.WriteConfig()
 }
