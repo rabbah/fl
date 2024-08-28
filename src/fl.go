b37aa3f8ebefa06b68cebcba7f551b0170e3f011
Rodric Rabbah <rodric.rabbah@postman.com>
2024-08-27 21:47:54 -0400

    Update fl flow.

--- src/fl.go
+++ src/fl.go
@@ -1,81 +1,14 @@
 package main
 
 import (
-	"fl/api"
-	"fl/auth"
+	"fl/api" // Add this line to import the auth package
 	"fl/cmd"
 	"fl/exec"
-	"fl/helpers"
-	"fl/io"
 	"fl/utils"
-	"fl/web"
 	"fmt"
 	"os"
 	"path/filepath"
 )
-
-func runFL(flags cmd.FlagConfig) {
-	helpers.Print(flags.Verbose, "Prompt extracted:", flags.Prompt)
-
-	// Validate executing user
-	helpers.Print(flags.Verbose, "Authorizing and sending prompt...")
-	result, msg, err := auth.ValidateUserGetCmd(flags.Prompt, flags.Langtool, flags.FLID)
-	if err != nil {
-		fmt.Printf("%s: %v\n", msg, err)
-		os.Exit(1)
-	}
-
-	// Emit the result
-	helpers.Print(flags.Verbose, "Output: \n")
-	fmt.Println(result)
-	fmt.Println()
-
-	// copy to clipboard
-	utils.Clip(result)
-
-	// check if explain flag, then call explain
-	if flags.Explain {
-		helpers.Print(flags.Verbose, "Sending command for explanation...")
-
-		explanation, err := web.ExplainCommand(result, flags.Langtool)
-		if err != nil {
-			fmt.Printf("Failed to call Flows API - %s: %v\n", explanation, err)
-			os.Exit(1)
-		}
-
-		fmt.Println(explanation)
-	}
-
-	// if not skipping prompt, ask user if they would like to execute
-	userExecute := false
-	if flags.PromptRun {
-		userExecute = exec.PromptExec()
-	}
-
-	// perform the command if autoexecute enabled or user prompted to exec
-	if (flags.AutoExecute && !flags.PromptRun) || userExecute {
-		helpers.Print(flags.Verbose, "Executing the result...")
-
-		Cmd := exec.Command(result)
-		out, err := Cmd.Exec()
-
-		if err != nil {
-			panic(err)
-		}
-
-		// Print the output
-		fmt.Println(out)
-	}
-
-	if flags.Outfile != "" {
-		err := io.Output(flags.Outfile, result)
-
-		if err != nil {
-			fmt.Printf("Failed save output to file: %s\n", err)
-			os.Exit(1)
-		}
-	}
-}
 
 func main() {
 	home, _ := os.UserHomeDir()
@@ -87,10 +20,13 @@ func main() {
 		panic(err)
 	}
 
-	cmd.ParseCommandLine(os.Args[1:], &flags)
+	err = cmd.ParseCommandLine(os.Args[1:], &flags)
+	if err != nil {
+		panic(err)
+	}
 
 	if flags.Config {
-		cmd.WriteConfig(filepath, flags)
+		cmd.WriteConfig(filepath, &flags)
 		os.Exit(0)
 	}
 
@@ -106,13 +42,63 @@ func main() {
 		}
 
 		flags.FLID = flid
-		newFlags := cmd.FlagConfig{}
-		err = cmd.ReadConfig(filepath, &newFlags)
+		cmd.WriteConfig(filepath, &flags)
+	}
+
+	utils.Log(flags.Verbose, "Flags: %+v\n", flags)
+
+	runFL(flags)
+}
+
+func runFL(flags cmd.FlagConfig) {
+	res, err := api.GenerateCommand(flags.Prompt, flags.Langtool, flags.FLID)
+	if err != nil {
+		fmt.Printf("Failed to generate a command: %v\n", err)
+		os.Exit(1)
+	}
+
+	utils.Clip(res.Cmd)
+	fmt.Println(res.Cmd)
+	fmt.Println()
+
+	if flags.Outfile != "" {
+		err = os.WriteFile(flags.Outfile, []byte(res.Cmd), 0755)
+		if err != nil {
+			fmt.Printf("Failed save output to file: %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	//TODO
+	/*
+		if flags.Explain {
+			explanation, err := web.ExplainCommand(result, flags.Langtool)
+			if err != nil {
+				fmt.Printf("Failed to call Flows API - %s: %v\n", explanation, err)
+				os.Exit(1)
+			}
+
+			fmt.Println(explanation)
+		}
+	*/
+
+	runIt := false
+	if flags.PromptRun && !flags.AutoExecute {
+		runIt = exec.PromptExec()
+	}
+
+	// perform the command if autoexecute enabled or user prompted to exec
+	if flags.AutoExecute || runIt {
+		utils.Log(flags.Verbose, "Executing the generated command...")
+
+		Cmd := exec.Command(res.Cmd)
+		out, err := Cmd.Exec()
+
 		if err != nil {
 			panic(err)
 		}
-		cmd.WriteConfig(filepath, newFlags)
-	}
 
-	runFL(flags)
+		// Print the output
+		fmt.Println(out)
+	}
 }
