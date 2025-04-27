6a2e2e1f1ed62c7981a8a9f92c1a30c3b02f62c4
Rodric Rabbah <rodric.rabbah@postman.com>
2025-04-26 22:42:19 -0400

    Update API endpoints.

--- src/api/conf.go
+++ src/api/conf.go
@@ -3,10 +3,11 @@ package api
 const (
 	GitHubClientID = "Ov23liak5XRTpeHgGDtx"
 
-	LoginGuestAPI           = "https://postman.flows.pstmn-beta.io/api/fl-login-guest"
-	LoginGitHubAPI          = "https://postman.flows.pstmn-beta.io/api/fl-login-github"
-	GenerateCmdAPI          = "https://postman.flows.pstmn-beta.io/api/fl-generate"
-	StartSubscriptionAPI    = "https://postman.flows.pstmn-beta.io/api/fl-subscription-start"
-	CancelSubscriptionAPI   = "https://postman.flows.pstmn-beta.io/api/fl-subscription-cancel"
-	StatusOfSubscriptionAPI = "https://postman.flows.pstmn-beta.io/api/fl-subscription-status"
+	baseURL                 = "https://macho-dropkick-stun.flows.pstmn.io/api"
+	LoginGuestAPI           = baseURL + "/default/fl-login-guest"
+	LoginGitHubAPI          = baseURL + "/default/fl-login-github"
+	GenerateCmdAPI          = baseURL + "/default/fl-generate"
+	StartSubscriptionAPI    = baseURL + "/default/fl-subscription-start"
+	CancelSubscriptionAPI   = baseURL + "/default/fl-subscription-cancel"
+	StatusOfSubscriptionAPI = baseURL + "/default/fl-subscription-status"
 )
